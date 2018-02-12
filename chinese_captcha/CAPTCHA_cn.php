1e5ab4474fcb855da7c47a7bd22c8c61167747ac
houan <houan@meili-inc.com>
2018-02-12 14:28:04 +0800

    review

--- chinese_captcha/CAPTCHA_cn.php
+++ chinese_captcha/CAPTCHA_cn.php
@@ -1,29 +1,29 @@
 <?php
 session_start();
 
-$image = imagecreatetruecolor(200,60);//新建画布
-$bgcolor = imagecolorallocate($image,250,250,200);//画布背景颜色
-imagefill($image,0,0,$bgcolor);//填充画布
+$image = imagecreatetruecolor(200, 60);//新建画布
+$bgcolor = imagecolorallocate($image, 250, 250, 200);//画布背景颜色
+imagefill($image, 0, 0, $bgcolor);//填充画布
 
 $fontface = 'STXINWEI.TTF';
-$strdb = array('小','桥','哈','呵');
+$strdb = array('小', '桥', '哈', '呵');//验证码字体库
 $captcha_code = '';//全局变量用于存储验证码内容
-for($i=0;$i<4;$i++){
-	$fontcolor = imagecolorallocate($image,rand(30,80),rand(30,80),rand(30,80));
-	$cn = $strdb[rand(0,3)];
-	$captcha_code .= $cn;
-	imagettftext($image,mt_rand(20,24),mt_rand(-60,60),(40*$i+20),mt_rand(30,35),$fontcolor,$fontface,$cn);
+for ($i = 0; $i < 4; $i++) {
+    $fontcolor = imagecolorallocate($image, rand(30, 80), rand(30, 80), rand(30, 80));
+    $cn = $strdb[rand(0, 3)];
+    $captcha_code .= $cn;
+    imagettftext($image, mt_rand(20, 24), mt_rand(-60, 60), (40 * $i + 20), mt_rand(30, 35), $fontcolor, $fontface, $cn);
 }//画布内容（中文）
 $_SESSION['authcode'] = $captcha_code;
 
-for($i = 0;$i<200;$i++){
-	$pointcolor = imagecolorallocate($image,rand(50,200) ,rand(50,200) ,rand(50,200));
-	imagesetpixel($image, rand(1,199), rand(1,59), $pointcolor);
+for ($i = 0; $i < 200; $i++) {
+    $pointcolor = imagecolorallocate($image, rand(50, 200), rand(50, 200), rand(50, 200));
+    imagesetpixel($image, rand(1, 199), rand(1, 59), $pointcolor);
 }//添加干扰点
 
-for($i = 0;$i<3;$i++){
-	$linecolor = imagecolorallocate($image,rand(80,220) ,rand(80,220) ,rand(80,220));
-	imageline($image, rand(1,199), rand(1,59), rand(1,199), rand(1,59),$linecolor);
+for ($i = 0; $i < 3; $i++) {
+    $linecolor = imagecolorallocate($image, rand(80, 220), rand(80, 220), rand(80, 220));
+    imageline($image, rand(1, 199), rand(1, 59), rand(1, 199), rand(1, 59), $linecolor);
 }//添加干扰线
 
 header('content-type: image/png');
--- image_captcha/CAPTCHA_img.php
+++ image_captcha/CAPTCHA_img.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
 $table = array(
-	'pic0'=>'狗',
-	'pic1'=>'猫',
-	'pic2'=>'鱼',
-	'pic3'=>'鸟',
+    'pic0' => '狗',
+    'pic1' => '猫',
+    'pic2' => '鱼',
+    'pic3' => '鸟',
 );
 
-$index = rand(0,3);
-$value = $table['pic'.$index];
+$index = rand(0, 3);
+$value = $table['pic' . $index];
 $_SESSION['authcode'] = $value;
-$filename = dirname(__FILE__).'\\pic'.$index.'.jpg';
+$filename = dirname(__FILE__) . '\\pic' . $index . '.jpg';
 $content = file_get_contents($filename);
 
 header('content-type:image/jpg');
